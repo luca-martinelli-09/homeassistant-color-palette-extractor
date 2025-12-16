fabc6723e69b016433bb33af58dad30f0c3dcc85
Luca Martinelli <martinelliluca98@gmail.com>
2025-12-16 18:29:43 +0100

    Initialize project

---
+++ custom_components/color_palette_extractor/const.py
@@ -0,0 +1,9 @@
+"""Constants for the color_palette_extractor component."""
+
+ATTR_PATH = "color_extract_path"
+ATTR_URL = "color_extract_url"
+
+DOMAIN = "color_palette_extractor"
+DEFAULT_NAME = "Color Palette extractor"
+
+SERVICE_TURN_ON = "turn_on"
